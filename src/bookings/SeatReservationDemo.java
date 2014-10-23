ee130ae5063316a7ff2395d50258351ea45fc125
Steven Henry <sch114@edge31.doc.ic.ac.uk>
2014-10-23 15:18:16 +0100

    modified:   bookings/SeatReservationDemo.java

--- src/bookings/SeatReservationDemo.java
+++ src/bookings/SeatReservationDemo.java
@@ -3,7 +3,14 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
-
+    	
+    	SeatReservationManager srm = new SeatReservationManager();
+    	
+    	srm.reserve(new Seat('D', 5) ,new Customer());
+    	
+    	srm.reserve(new Seat('E', 6) ,new Customer());
+    	
+    	srm.reserve(new Seat('F', 7) ,new Customer());
     }
     
 }
